b835373f4298bfb626e44ebb37d3d020e7e0d347
lsantillana <letysf18@live.com>
2023-05-02 21:27:31 -0700

    Data averages for Student Finances section

--- student_finances.R
+++ student_finances.R
@@ -14,10 +14,10 @@
 # rm(list = ls(all.names = TRUE))
 
 # install and load packages
-install.packages("questionr")
+#install.packages("questionr")
 library(tidyverse)
 library(dplyr)
-library(questionr) #for processing survey data
+#library(questionr) #for processing survey data
 
 # read in raw data
 survey <- read.csv("SEFS Graduate Student Finances Survey_49 responses.csv")
@@ -25,3 +25,12 @@ survey <- read.csv("SEFS Graduate Student Finances Survey_49 responses.csv")
 # subset your data for only student finances
 
 student_fin <- survey[8:25]
+
+#str(student_fin)
+#colnames(student_fin)
+
+data_means <- student_fin %>% select(2, 4:7, 11:12, 16:17) %>% summarise(across(everything(), mean, na.rm = TRUE))
+  ## data points edited in excel to remove special characters and letters
+  ## some data points were ranges--these were averaged 
+
+
